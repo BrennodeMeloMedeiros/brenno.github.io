f372efde0e84835a7cd242548949603cf97f70c5
BrennoMelo <brennoprogmm@gmail.com>
2021-03-05 19:33:19 -0300

    first commit

---
+++ JavaScript/index.js
@@ -0,0 +1,49 @@
+
+const languageButton = document.querySelector('#LanguageButton');
+const languageCheckbox = document.querySelector('#Language')
+let languageChecked = languageCheckbox.checked
+
+let wantedLanguage = "english"
+languageButton.addEventListener('click', ()=>{
+    // EN = FALSE | PT-BR = TRUE 
+    if(!languageChecked){
+        languageChecked = true;
+        languageButton.className = "PT-BR";
+        wantedLanguage = "portuguese";
+    }else{
+        languageChecked = false;
+        languageButton.className = "EN";
+        wantedLanguage = "english";
+    }
+    text = data[wantedLanguage];
+
+    for(id of Object.keys(text)){
+        content = text[id];
+        targetElement = document.getElementById(`${id}`);
+        targetElement.innerText = content;
+        
+    }
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const boxes = document.querySelectorAll('article.Box');
+
+
+boxes.forEach((box)=>{
+    box.addEventListener('mouseover', ()=>{
+        box.style.backgroundColor = "tomato"
+    })
+})
---
+++ ideias.md
@@ -0,0 +1,13 @@
+## Show
+- Technologys 
+- About me 
+    - Language (And change the language between Portuguese and English)
+    - Contact 
+    - Personal Afirmation
+- Projects #
+
+## == Language
+    
+
+
+ 
